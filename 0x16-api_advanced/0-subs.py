e9c2b1fc4f1d34906cec10db489f250a0dff6acd
Abexholy <abexholy123@.email.com>
2024-06-21 20:16:14 -0700

    code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,19 @@
 #!/usr/bin/python3
 """
-Module 0-subs that contains the function number_of_subscribers
+Script that queries subscribers on a given Reddit subreddit.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the number of
-    subscribers (not active users, total subscribers) for a given
-    subreddit. If an invalid subreddit is given, the function should
-    return 0)
-    """
-    if not subreddit or type(subreddit) is not str:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/your_username)'}
-    
-    try:
-        r = requests.get(url, headers=headers, allow_redirects=False)
-        if r.status_code == 200:
-            data = r.json()
-            return data.get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-    except requests.exceptions.RequestException as e:
-        return 0
-
