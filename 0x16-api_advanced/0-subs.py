e09f68ff1e03dc3528691a82e393ca69b9ddcc99
Abexholy <abexholy123@.email.com>
2024-06-21 19:30:52 -0700

    python

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 """
-this Contain number_of_subscribers
+Module 0-subs that contains function number_of_subscribers
 """
-
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ it return number of subscribers for thesubreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given
+    subreddit. If an invalid subreddit is given, the function should
+    return 0)
+    """
+    if not subreddit or type(subreddit) is not str:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0'}
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        r = r.json()
+    else:
+        return 0
+    return r.get('data', {}).get('subscribers', 0)
